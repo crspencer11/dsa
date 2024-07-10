11e48b9e3e8f2f233c8404cf62c55dc61a6a3e9a
Lil Blick <chrispencer@berkeley.edu>
2024-07-10 16:10:09 -0700

    add merge 2 sorted lists

---
+++ leetcode/easy/lc_21.py
@@ -0,0 +1,20 @@
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        head = ListNode()
+        curr = head
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = ListNode(list1.val)
+                list1 = list1.next
+            else:
+                curr.next = ListNode(list2.val)
+                list2 = list2.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        return head.next
