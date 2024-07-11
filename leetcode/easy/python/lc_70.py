75bdaad715578ebc8914ea932eaf78a34dcc73dd
Lil Blick <chrispencer@berkeley.edu>
2024-07-10 23:26:35 -0700

    change lc 70

--- leetcode/easy/python/lc_70.py
+++ leetcode/easy/python/lc_70.py
@@ -1,12 +1,12 @@
 def climbStairs(n: int) -> int:
-        return self.memoize(n)
+        return memoize(n)
 
-    def memoize(self, num: int, memo={}):
-        if num in memo:
-            return memo[num]
-        if num == 0 or num == 1:
-            return 1
-        else:
-            result = self.climbStairs(num-1) + self.climbStairs(num-2)
-            memo[num] = result
-            return result
+def memoize(num: int, memo={}):
+    if num in memo:
+        return memo[num]
+    if num == 0 or num == 1:
+        return 1
+    else:
+        result = climbStairs(num-1) + climbStairs(num-2)
+        memo[num] = result
+        return result
