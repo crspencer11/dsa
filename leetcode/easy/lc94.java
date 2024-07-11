c0b48579f77bac8c0226272861de0ed0a3c7beea
Lil Blick <chrispencer@berkeley.edu>
2024-07-10 17:29:04 -0700

    add lc 94

---
+++ leetcode/easy/lc94.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> results = new ArrayList<>();
+        traverse(root, results);
+        return results;
+    }
+
+    private void traverse(TreeNode root, List<Integer> results) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left, results);
+        results.add(root.val);
+        traverse(root.right, results);
+    }
+}
