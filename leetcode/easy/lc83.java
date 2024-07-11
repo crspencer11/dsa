9e3dbc9b4c8cf6284ec32d93e58b56b4a9067ff0
Lil Blick <chrispencer@berkeley.edu>
2024-07-10 17:22:27 -0700

    add lc 83

---
+++ leetcode/easy/lc83.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            if (prev != null && prev.val == curr.val) {
+                prev.next = curr.next;
+            }
+            else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+}
