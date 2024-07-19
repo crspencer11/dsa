d7302ec302843728152b1a73f8b1f9b2fb36bb02
Lil Blick <chrispencer@berkeley.edu>
2024-07-18 17:18:56 -0700

    add lc 257

---
+++ leetcode/easy/java/lc257.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) {
+            return paths;
+        }
+        dfs(root, String.valueOf(root.val), paths);
+        return paths;
+    }
+    
+    private void dfs(TreeNode node, String path, List<String> paths) {
+        if (node.left == null && node.right == null) {
+            paths.add(path);
+            return;
+        }
+        if (node.left != null) {
+            dfs(node.left, path + "->" + node.left.val, paths);
+        }
+        if (node.right != null) {
+            dfs(node.right, path + "->" + node.right.val, paths);
+        }
+    }
+}
