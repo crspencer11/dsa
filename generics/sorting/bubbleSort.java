bd88549f2af8dba391b3c5b31551d70a2677cb54
Lil Blick <chrispencer@berkeley.edu>
2024-10-16 15:21:23 -0700

    optimize using bool flipped value

--- generics/sorting/bubbleSort.java
+++ generics/sorting/bubbleSort.java
@@ -5,12 +5,18 @@ class Main {
     int size = nums.length;
 
     for(int i=0; i<(size-1); i++) {
+      boolean swapped = false;
+
       for(int j=0; j<(size-i-1); j++) {
         if(nums[j] > nums[j+1]) {
 	  int temp = nums[j];
 	  nums[j] = nums[j+1];
 	  nums[j+1] = temp;
+	  swapped = true;
 	}
+      }
+      if(!swapped) {
+        break;
       }
     }
   }
