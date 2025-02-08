d2c9d2f623aded89abdfd4637806da4470b0b11b
Lil Blick <chrispencer@berkeley.edu>
2025-02-07 20:27:36 -0800

    add bsearch java impl lc2239

--- leetcode/easy/java/lc2239.java
+++ leetcode/easy/java/lc2239.java
@@ -18,3 +18,28 @@ class Solution {
     }
 }
 
+// bsearch impl
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        int closest = nums[0];
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == 0) return 0; 
+            if (Math.abs(nums[mid]) < Math.abs(closest) || 
+                (Math.abs(nums[mid]) == Math.abs(closest) && nums[mid] > closest)) {
+                closest = nums[mid];
+            }
+            if (nums[mid] < 0) {
+                left = mid + 1; 
+            } else {
+                right = mid - 1; 
+            }
+        }
+        return closest;
+    }
+}
+
+
