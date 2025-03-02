db2375a96c1771df5b5d758234ef8cb3f010e562
Lil Blick <chrispencer@berkeley.edu>
2025-03-01 20:49:00 -0800

    lc_703.py

---
+++ leetcode/easy/python/lc_703.py
@@ -0,0 +1,14 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = nums
+
+    def add(self, val: int) -> int:
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
