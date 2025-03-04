c271d63831efcb2d86430d090aa66e8e29f8edb7
Lil Blick <chrispencer@berkeley.edu>
2025-03-04 15:52:19 -0800

    py impl lc_2570: merge two arrays by summing values

---
+++ leetcode/easy/python/lc_2570.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        i = 0
+        j = 0
+        final = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] == nums2[j][0]:
+                final.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+            elif nums1[i][0] < nums2[j][0]:
+                final.append(nums1[i])
+                i += 1
+            else:
+                final.append(nums2[j])
+                j += 1
+        while i < len(nums1):
+            final.append(nums1[i])
+            i += 1
+        while j < len(nums2):
+            final.append(nums2[j])
+            j += 1
+        return final
+
