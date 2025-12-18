78fb69fd2f1fdeb95d0f525ae97151e145c99c56
Lil Blick <chrispencer@berkeley.edu>
2025-12-18 13:50:13 -0800

    hashMap impl

---
+++ generics/dataStructures/hashMap.py
@@ -0,0 +1,17 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.hm = {}
+
+    def put(self, key: int, value: int) -> None:
+        self.hm[key] = value
+
+    def get(self, key: int) -> int:
+        if key in self.hm:
+            return self.hm[key]
+        return -1
+
+    def remove(self, key: int) -> None:
+        if key in self.hm:
+            self.hm.pop(key)
+
