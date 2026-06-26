f80d09b6e903007f57489b8bd7600a91dfe3d88d
Lil Blick <chrispencer@berkeley.edu>
2026-06-26 10:19:08 -0700

    1732. Find the Highest Altitude

---
+++ leetcode/easy/python/lc1732.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestAltitude(self, gain: list[int]) -> int:
+        curr_alt = 0
+        max_alt = 0
+        for g in gain:
+            curr_alt += g
+            if curr_alt > max_alt:
+                max_alt = curr_alt
+        return max_alt
